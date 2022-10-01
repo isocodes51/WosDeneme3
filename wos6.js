6c09504f8a3d8188dd58e575dab56c8c1c4d986c
isocodes51 <flyingcow51@hotmail.com>
2022-10-02 01:42:59 +0300

    02.10.2022 01:42

--- wos6.js
+++ wos6.js
@@ -1,32 +1,26 @@
 const url="https://wos-api.clarivate.com/api/woslite/?databaseId=WOS&usrQuery=TS%3D%28cadmium%29&count=1&firstRecord=1"; 
-function getJSON(){
+ ( ()=>{
     fetch(url,{
-        method: 'GET',
         mode:"no-cors",
+        method: 'GET',
         headers: {
             'accept': 'application/json',
-            'X-ApiKey': '6b46ffda86a7a3e9aa2336cd846bfd5f09e18708'
+            'X-ApiKey': '8b88d28c52323732785d0490e30b0ff42986d01d'
         }
-
-        
-        
-            
     })
+    
     .then((response)=>{
-    if(response.status===0)
         return response.json();
-    else
-      console.log("HATA HATA");  
     })
+
     .then((Data)=>{
       console.log("My data: "+Data+"\n");
-      
      })
     
      .catch((err)=>{
        console.log("My Hata: "+err);
      })
     
-}
-getJSON(); 
+})();
+
 
